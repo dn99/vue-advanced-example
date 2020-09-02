13f800775df4b0a2fda001144ce08eaaa0f573ef
Hansik Kim <dn99dn@gmail.com>
2020-09-02 21:16:03 +0900

    vue-advanced 실습중

---
+++ src/views/NewsView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div v-for="news in this.$store.state.news">{{ news.title }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        this.$store.dispatch('FETCH_NEWS');
+    }
+}
+</script>
+
+<style>
+
+</style>
